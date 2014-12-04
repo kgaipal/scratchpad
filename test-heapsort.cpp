e90d10f6e1c9d6a5b5668ec55f1afe336c5ba6c2
Kshitij Gaipal <kgaipal@gmail.com>
2014-12-04 15:51:12 -0600

    heapsort now working as expected

--- test-heapsort.cpp
+++ test-heapsort.cpp
@@ -28,59 +28,53 @@ void build_heap(int array[], unsigned int size)
 	}
 }
 
-void max_heapify(int array[], unsigned int start, unsigned int size)
+void max_heapify(int array[],
+		 unsigned int start,
+		 unsigned int size)
 {
-	if (array == nullptr) {
-		return;
-	}
-
-	// const unsigned int size = sizeof(array)/sizeof(array[0]);
 	const unsigned int parent = start;
 	const unsigned int left_child = 2*parent + 1;
 	const unsigned int right_child = 2*parent + 2;
 	
 	unsigned int largest = parent;
 
-	if (left_child < size && array[left_child] > array[largest]) {
+	if (left_child < size
+	    && array[left_child] > array[largest]) {
 		largest = left_child;
 	}
 
-	if (right_child < size && array[largest] < array[right_child]) {
+	if (right_child < size
+	    && array[largest] < array[right_child]) {
 		largest = right_child;
 	}
 
 	// parent was not the largest of three
 	if (parent != largest) {
 		swap(&array[largest], &array[parent]);
-		max_heapify(array, largest, size);
+		max_heapify(array, parent, size);
 	}
 }
 
 void heapsort(int array[], unsigned int size)
 {
-	if (array == nullptr) {
-		return;
-	}
-
 	// build the heap for first time; O(n) complexity
 	build_heap(array, size);
 
-	std::cout << "bh: ";
-	print_array(array, size);
-
-	swap(&array[0], &array[size-1]);
 	// top element is max, shrink the array size and work on the remaining
-	for (int i = size-1 ; i > 0; i--) {
-		std::cout << i << "|";
+	unsigned int end = size-1;
+	while (end > 0) {
+		std::cout << end << "|";
 		print_array(array, size);
-		swap(&array[0], &array[i]);
-		max_heapify(array, 0, i-1);
+
+		swap(&array[0], &array[end]);
+		end--;
+		max_heapify(array, 0, end+1);
 	}
 }
 
 int main()
 {
-	int array[] = {2, 1, 0, 5, 4, 10, 6};
+	int array[] = {2, 1, 0, 5, 4, 10, 6, -1};
 	unsigned int size = sizeof(array)/sizeof(array[0]);
 
 	std::cout << "before:";
