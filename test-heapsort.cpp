35cc5acea23634ebf67d4dad42a1b9c38b21c2f0
Kshitij Gaipal <kgaipal@bomgar.com>
2014-12-04 14:39:40 -0600

    added heap sort [not working] and cleanup

---
+++ test-heapsort.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+void build_heap(int array[])
+{
+}
+
+void max_heapify(int array[], unsigned int size)
+{
+}
+
+int main()
+{
+	int array[] = {-2, 1, 0, 5, 4, 10, 6};
+	heapsort(array, sizeof(array)/array[0]);
+	
+	return 0;
+}
+	      
