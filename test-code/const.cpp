85e166f64f3c68f2adba640a7848c11e51a735ac
Kshitij Gaipal <kgaipal@gmail.com>
2016-02-09 14:06:39 -0600

    [test code] const example code

--- test-code/const.cpp
+++ test-code/const.cpp
@@ -1,65 +1,42 @@
 #include <iostream>
 
-int foo()
-{
-	int var = 4;
-	return var;
-}
-
 void func1()
 {
-	const int& s = foo();
-	std::cout << s << std::endl;
-}
-
-void func2()
-{
-	int N = 5;
-	for (int m = -5; m < N; m++) {
-		std::cout << m%N << " ";
-	}
-
-	std::cout << -7%8;
-}
-
-void func3()
-{
-	long l_type = -1;
-	std::cout << l_type << " size:" << sizeof(l_type) << std::endl;
-
-	int i_type = -2;
-	std::cout << i_type << " size:" << sizeof(i_type) << std::endl;
-}
-
-void func4()
-{
-	class A
-	{
+	class A {
 	public:
-		void f1() const { ; }
-		void f2() { ; }
+		void const_f() const {
+			std::cout << "const f()\n";
+		}
+
+		void f() {
+			std::cout << "f()\n";
+		}
 	};
 
-	A a;
-	const A& a_ref = a;
-	const A& aa_ref = a_ref;
+	// behave like a reference
+	{
+		A * const a = new A(); // const pointer to A (read 'const a')
+		a->const_f();
 
-	a.f1();
-	a_ref.f1();
-	aa_ref.f1();
+		a->f();
+		// a = new A();  // compile error
+	}
 
-	a.f2();
-	// a_ref.f2();		// error: cant invoke non-const function
-	// aa_ref.f2();		// error: cant invoke non-const function
+	// behaves like a normal variable but can call only const functions
+	{
+		const A * a = new A(); // pointer to a const A (read 'const A')
+		a->const_f();
 
-	const A* const a_ptr = &a;
-	a_ptr->f1();
-	// a_ptr->f2();		// error: cant invoke non-const function from non-const ptr
+		// a->f(); // compile error
+		a = new A();
+	}
+	
+	std::cout << std::endl;
 }
 
 int main()
 {
-	// func4();
+	func1();
 
 	return 0;
 }
