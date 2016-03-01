9067607eec544badc499520df6eee47d8f2814ef
Kshitij Gaipal <kgaipal@gmail.com>
2016-03-01 15:22:10 -0600

    [test code] added unorderd_map for checking load_factor and bucket sizes for each key
    observation: a string key most likely cause collisions than integeral value!

--- test-code/unordered-map.cpp
+++ test-code/unordered-map.cpp
@@ -5,6 +5,22 @@
 #include <string>
 #include <unordered_map>
 #include <memory>
+#include <sstream>
+
+#define ELEMENTS_COUNT 100
+const std::string convert_to_string(int i) {
+	std::stringstream ss;
+	ss << i;
+	return ss.str();
+}
+
+unsigned convert_to_int(unsigned i) {
+	return i;
+}
+
+#define CONVERT_TO_TYPE(val) { convert_to_int(val) }
+
+typedef std::unordered_map<unsigned, int> UNORDERED_MAP;
 
 class Element
 {
@@ -41,26 +57,87 @@ private:
 	std::string string_value;
 };
 
-int main()
+void check_bucket_size(UNORDERED_MAP& uom)
 {
-	// Element s(0, "element s");
-	// Element another = s;
+	std::cout << "\nbucket_size()" << uom.bucket_count() << "\n";
 
-	// std::unordered_map<int, std::string> uomap;
-	// std::map<int, std::string> uomap;
-	// uomap.emplace(1,"string value 1");
-	// uomap.emplace(1, 1,"string value 1");
-	// uomap.emplace(2, 2 ,"string value 2");
-	// uomap.emplace(3, 3 ,"string value 3");
-	std::unique_ptr<Element> e;
-	if (e.get()) {
-		std::cout << "unique_ptr is non-empty\n";
-	} else {
-		std::cout << "unique_ptr is empty\n";
+	for (int i = 0; i < ELEMENTS_COUNT; i++) {
+		std::cout
+			<< "key " << i
+			<< " is in bucket# " << uom.bucket(CONVERT_TO_TYPE(i))
+			<< "\n";
 	}
 
-	std::unordered_map<int, std::unique_ptr<Element> > uomap;
-	// uomap.insert(std::make_pair<int, Element*>(0, Element(1, "string value 1")));
-	uomap[0].reset(new Element(1, "string value 1"));
+	for (unsigned i = 0u; i < uom.bucket_count(); i++) {
+		if (uom.bucket_size(i) < 1) {
+			continue;
+		}
+		std::cout << "\nbucket# " << i
+			  << " has " << uom.bucket_size(i) << " elements";
+	}
+}
+
+void check_load_factor(UNORDERED_MAP& uom)
+{
+	auto print_stats = [&uom] (const std::string& message = "") {
+		std::cout << "\n[" << message << "]";
+		std::cout << "\tload_factor:" << uom.load_factor();
+		std::cout << "\tbucket_count:" << uom.bucket_count();
+		std::cout << "\tsize:" << uom.size();
+	};
+
+	print_stats("before_insert");
+	for (int i = 0; i < ELEMENTS_COUNT; i++) {
+		uom[CONVERT_TO_TYPE(i)] = 2*i;
+		if (uom.load_factor() > 0.0) {
+			std::stringstream ss;
+			ss << "middle-" << i;
+			print_stats(ss.str());
+		}
+	}
+	print_stats("after_insert");
+
+	check_bucket_size(uom);
+	std::cout << "\n=============";
+
+	print_stats("before_remove");
+	for (int i = 0; i < ELEMENTS_COUNT; i++) {
+		uom.erase(CONVERT_TO_TYPE(i));
+		if (uom.load_factor() > 0.8) {
+			std::stringstream ss;
+			ss << "middle-" << i;
+			print_stats(ss.str());
+		}
+	}
+	print_stats("after_remove");
+
+}
+
+int main()
+{
+	// // Element s(0, "element s");
+	// // Element another = s;
+
+	// // std::unordered_map<int, std::string> uomap;
+	// // std::map<int, std::string> uomap;
+	// // uomap.emplace(1,"string value 1");
+	// // uomap.emplace(1, 1,"string value 1");
+	// // uomap.emplace(2, 2 ,"string value 2");
+	// // uomap.emplace(3, 3 ,"string value 3");
+	// std::unique_ptr<Element> e;
+	// if (e.get()) {
+	//	std::cout << "unique_ptr is non-empty\n";
+	// } else {
+	//	std::cout << "unique_ptr is empty\n";
+	// }
+
+	// std::unordered_map<int, std::unique_ptr<Element> > uomap;
+	// // uomap.insert(std::make_pair<int, Element*>(0, Element(1, "string value 1")));
+	// uomap[0].reset(new Element(1, "string value 1"));
+
+	UNORDERED_MAP uom;
+	check_load_factor(uom);
+
+	std::cout << std::endl;
 	return 0;
 }
