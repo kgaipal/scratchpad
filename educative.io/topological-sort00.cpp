2936daf87166f2152e329e9415b7432a68144387
Kshitij Gaipal <kgaipal@gmail.com>
2019-10-16 12:01:59 -0700

    formatting

--- educative.io/topological-sort00.cpp
+++ educative.io/topological-sort00.cpp
@@ -62,22 +62,19 @@ public:
 };
 
 int main(int argc, char* argv[]) {
-    vector<int> result =
-        TopologicalSort::sort(4, vector<vector<int>>{vector<int>{3, 2}, vector<int>{3, 0}, vector<int>{2, 0}, vector<int>{2, 1}});
+    vector<int> result = TopologicalSort::sort(4, vector<vector<int>> {vector<int>{3, 2}, vector<int>{3, 0}, vector<int>{2, 0}, vector<int>{2, 1}});
     for (auto num : result) {
         cout << num << " ";
     }
     cout << endl;
 
-    result = TopologicalSort::sort(
-        5, vector<vector<int>>{vector<int>{4, 2}, vector<int>{4, 3}, vector<int>{2, 0}, vector<int>{2, 1}, vector<int>{3, 1}});
+    result = TopologicalSort::sort(5, vector<vector<int>>{vector<int>{4, 2}, vector<int>{4, 3}, vector<int>{2, 0}, vector<int>{2, 1}, vector<int>{3, 1}});
     for (auto num : result) {
         cout << num << " ";
     }
     cout << endl;
 
-    result = TopologicalSort::sort(
-        7, vector<vector<int>>{vector<int>{6, 4}, vector<int>{6, 2}, vector<int>{5, 3}, vector<int>{5, 4}, vector<int>{3, 0}, vector<int>{3, 1}, vector<int>{3, 2}, vector<int>{4, 1}});
+    result = TopologicalSort::sort(7, vector<vector<int>>{vector<int>{6, 4}, vector<int>{6, 2}, vector<int>{5, 3}, vector<int>{5, 4}, vector<int>{3, 0}, vector<int>{3, 1}, vector<int>{3, 2}, vector<int>{4, 1}});
     for (auto num : result) {
         cout << num << " ";
     }
